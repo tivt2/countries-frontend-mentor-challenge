bd107b5142caa74c1774121f26b67d9daccaedf2
tivt <tivt231@gmail.com>
2023-07-10 19:17:21 -0300

    feat: basic setup of font and colors + lucide icon pack install

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,11 @@
-import './styles/main.css';
+import '../styles/main.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Nunito_Sans } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const nunitoSans = Nunito_Sans({
+  weight: ['300', '600', '800'],
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={nunitoSans.className}>{children}</body>
     </html>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'base-100': 'hsl(0, 0%, 100%)',
+        'base-200': 'hsl(0, 0%, 98%)',
+        'base-300': 'hsl(0, 0%, 52%)',
+        'base-400': 'hsl(209, 23%, 22%)',
+        'base-500': 'hsl(207, 26%, 17%)',
+        'base-600': 'hsl(200, 15%, 8%)',
+      },
+    },
   },
   plugins: [],
 };
